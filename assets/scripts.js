7eece605e5875f7a1cb69b522b48ab9dd982ecc3
erwan <erwan-14730@outlook.fr>
2023-10-08 11:54:34 +0200

    Ajout de srcset à toutes les images avec une ou plusieurs variante de taille, lazyload, conversion de toutes les images en webp. Perf supérieure a 90 lors au format ordinateur lors de l'audit lighthouse

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,18 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
 });
+
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.expand = 500; // Ajustez selon vos besoins
